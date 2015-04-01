4e2a4ef5f9c1852fe3586054f7212172d717e1e9
John Sanders <sandersjohnr@gmail.com>
2015-04-01 14:49:21 -0400

    backbone

--- public/js/models/collab_artist.js
+++ public/js/models/collab_artist.js
@@ -1,6 +1,6 @@
 console.log('Loaded: models/collab_artist.js');
 
-App.Models.CollabArtist = Backbone.Model.extract({
+App.Models.CollabArtist = Backbone.Model.extend({
   initialize: function() {
     console.log('Created: CollabArtist Model');
   }
--- public/js/models/main_artist.js
+++ public/js/models/main_artist.js
@@ -1,6 +1,6 @@
 console.log('Loaded: models/main_artist.js');
 
-App.Models.MainArtist = Backbone.Model.extract({
+App.Models.MainArtist = Backbone.Model.extend({
   initialize: function() {
     console.log('Created: MainArtist Model');
   }
