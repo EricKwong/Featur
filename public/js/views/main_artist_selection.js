053117e2b15fc51f76f7caa7a6485511388ec487
Eric Kwong <erickwong618@yahoo.com>
2015-04-02 15:42:04 -0400

    edited views

---
+++ public/js/views/main_artist_selection.js
@@ -0,0 +1,7 @@
+console.log('Loaded: views/main_artist_selection.js');
+
+App.Views.MainArtistSelection = Backbone.View.extend({
+  initialize: function() {
+    console.log('Created: MainArtistSelection View');
+  }
+});
