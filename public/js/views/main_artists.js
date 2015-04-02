9e9d8c145cff961644474c159c5b6ee98112019b
Eric Kwong <erickwong618@yahoo.com>
2015-04-02 17:30:41 -0400

    fixed search results emptying issue

--- public/js/views/main_artists.js
+++ public/js/views/main_artists.js
@@ -14,17 +14,18 @@ App.Views.MainArtists = Backbone.View.extend({
   },
 
   render: function() {
-  	this.collection.each(this.renderOne);
+    this.$('#main-artist-search-results').empty();
+    this.collection.each(this.renderOne);
   },
 
   renderOne: function(artist) {
-  	var artistSelection = new App.Views.MainArtistSelection({ model: artist });
-  	this.$('#main-artist-search-results').append(artistSelection.$el);
+    var artistSelection = new App.Views.MainArtistSelection({ model: artist });
+    this.$('#main-artist-search-results').append(artistSelection.$el);
   },
 
   searchArtist: function() {
     var searchInput = this.$('#search-input').val();
-
+    this.$('#search-input').val("");
     this.collection.url = '/search_artists/' + encodeURI(searchInput);
     this.collection.fetch({reset: true});
   }
