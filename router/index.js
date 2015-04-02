368f184ee6ba4478bcca66aa0751e6fe9904ee09
Eric Kwong <erickwong618@yahoo.com>
2015-04-02 12:15:34 -0400

    fixed merge conflicts

--- router/index.js
+++ router/index.js
@@ -1,9 +1,4 @@
 module.exports = function(app) {
 	app.use('/search_artists', require('./routes/artists'));
-<<<<<<< HEAD
-	app.use('/get_tracks', require('./routes/tracks'));
-=======
-  app.use('/get_artist_albums', require('./routes/artist_albums'));
-  app.use('/album_tracks', require('./routes/album_tracks'));
->>>>>>> 811d11f285beb66836a40ba51cce9d6a40c9651e
+	app.use('/get_collabs', require('./routes/artist_albums'));
 };
