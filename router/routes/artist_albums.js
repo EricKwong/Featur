07b25219a98d53c96f7fb8fcf154ee2834ce041b
John Sanders <sandersjohnr@gmail.com>
2015-04-02 16:26:50 -0400

    added artist image URLs to collabArtist objects

--- router/routes/artist_albums.js
+++ router/routes/artist_albums.js
@@ -6,10 +6,10 @@ var express    = require('express'),
 router.use(logger('dev'));
 var spotify = new SpotifyApi();
 
+// Router to get all tracks for collaborating artists, based on main artist ID
 router.get('/:artist_id', function (req, res) {
   spotify.getArtistAlbums(req.params.artist_id)
     .then(function (data) {
-      // console.log(data.body.items)
       var albums = data.body.items;
       return albums.map(function (album) { 
         return album.id; 
@@ -17,16 +17,16 @@ router.get('/:artist_id', function (req, res) {
     })
     .then(function (albumIds) {
       return spotify.getAlbums(albumIds)
-      // res.send(albumIds);
     })
     .then(function (albumData) {
-
+      
+      // Create nested object of all tracks for all albums of main artist
       var albums = albumData.body.albums;
       var albumsWithTracks = albums.map(function (album) {
         return album.tracks.items;
       });
 
-
+      // Create one dimensional array of all tracks
       var trackArray = [];
       albumsWithTracks.forEach(function (album) {
         album.forEach(function (track){
@@ -34,9 +34,9 @@ router.get('/:artist_id', function (req, res) {
         });
       });
 
-
+      // Create objects linking each artist and each track for each track that artists
+      // collaborated on
       var artistAndTrack = [];
-
       trackArray.forEach(function(track) {
         track.artists.forEach(function(artist) {
 
@@ -54,10 +54,9 @@ router.get('/:artist_id', function (req, res) {
         });
       });
 
-
+      // Transform linking object array into array with one object for each object,
+      // containing array of all track objects for that artist 
       var collabArtists = [artistAndTrack[0]];
-
-
       for (var i = 1; i < artistAndTrack.length; i++) {
         var artId = artistAndTrack[i].artistId;
         var currentTrack = artistAndTrack[i].track[0];
@@ -75,52 +74,32 @@ router.get('/:artist_id', function (req, res) {
       
       };
 
-      var collabArtistsWithImg = [];
+      // Helper function for dealing with undefined image urls
+      var undefinedCheck = function(image) {
+        if (image !== undefined) {
+          return image.url;
+        } else {
+          return "http://newton.physics.uiowa.edu/~sbaalrud/empty_profile.gif";
+        }
+      };
 
+      // Call to Spotify API to get Artist Url
+      spotify.getArtists(collabIds)
+              .then(function (artists) {
+                var artistArray = artists.body.artists;
+                for (var i = 0; i < artistArray.length; i++) {
+                  var imgUrl = undefinedCheck(artistArray[i].images[1]);
+                  collabArtists[i].artistImg = imgUrl;
 
-      for (var j = 0; j < collabArtists.length; j++) {
-        spotify.getArtist(collabArtists[j].artistId)
-               .then(function(artistInfo) {
-                // res.send(artistInfo)
-                  var undefinedCheck = function(image) {
-                    if (image !== undefined) {
-                      return image.url;
-                    } else {
-                      return "http://newton.physics.uiowa.edu/~sbaalrud/empty_profile.gif";
-                    }
-                  };
-
-                  var imgUrl = undefinedCheck(artistInfo.body.images[0]);
-                  // console.log(imgUrl);
-                  res.send(collabArtists[j]);
-                  collabArtists[j].artistImg = imgUrl;
-                  // console.log(collabArtists[j]);
-                  // collabArtistsWithImg.push(collabArtists[j]);
-               });
-      }
-
-      // res.send(collabArtistsWithImg);
-
-      // res.send(collabArtists);
-
-    });
+                };
       
+              // Send response
+              res.send(collabArtists); 
+              }); // END .then()
+
+    }); // END FINAL .then()
       
-      // }
-        // spotify.getArtist(track.artistId)
-          // var imageUrl = artist.images[1];
-          
-          // var undefinedCheck = function(image) {
-          //   if (image !== undefined) {
-          //     return image.url;
-          //   } else {
-          //     return "http://newton.physics.uiowa.edu/~sbaalrud/empty_profile.gif";
-          //   }
-          // };
-          
-
-
-});
+}); // END ROUTER
 
 module.exports = router;
 
