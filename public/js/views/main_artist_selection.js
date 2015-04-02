7cabee8842f3338f0cebc9115be7effef1894b50
Emily Lordahl <contact@emilylordahl.com>
2015-04-02 16:09:17 -0400

    added main artist selection template and view. added main artists collection view

--- public/js/views/main_artist_selection.js
+++ public/js/views/main_artist_selection.js
@@ -1,7 +1,11 @@
 console.log('Loaded: views/main_artist_selection.js');
 
 App.Views.MainArtistSelection = Backbone.View.extend({
+
   initialize: function() {
     console.log('Created: MainArtistSelection View');
-  }
+  	this.mainArtistTemplate = Handlebars.compile($('#main-artist-selection-template').html());
+  },
+
+
 });
--- public/js/views/main_artists.js
+++ public/js/views/main_artists.js
@@ -1,8 +1,19 @@
 console.log('Loaded: views/main_artists.js');
 
 App.Views.MainArtists = Backbone.View.extend({
+
+	el: '#main-artist-search-results',
+
   initialize: function() {
     console.log('Created: MainArtists Coll View');
-  }
-  
+  },
+
+  render: function() {
+  	this.collection.each(this.renderOne.bind(this));
+  },
+
+  renderOne: function(artist) {
+  	var artistSelection = new App.Views.MainArtistSelection({ model: artist });
+  	this.$el.append(artistSelection.$el);
+  }  
 });
