cec605aefa348e09064f185d813b009310283420
Eric Kwong <erickwong618@yahoo.com>
2015-04-03 14:39:49 -0400

    added songs route

--- router/index.js
+++ router/index.js
@@ -1,4 +1,5 @@
 module.exports = function(app) {
 	app.use('/search_artists', require('./routes/search_artists'));
 	app.use('/get_collabs', require('./routes/get_collabs'));
+	app.use('/songs', require('./routes/songs'));
 };
---
+++ router/routes/songs.js
@@ -0,0 +1,18 @@
+var express    = require('express'),
+	  router     = express.Router(),
+	  logger     = require('morgan'),
+	  bodyParser = require('body-parser'),
+	  models     = require('./models');
+
+var Song = models.songs;
+
+router.use(logger('dev'));
+router.use(bodyParser());
+
+router.get('/', function (req, res) {
+	Song
+		.create(req.body)
+		.then(function(newSong) {
+			res.send(newSong);
+		});
+});
