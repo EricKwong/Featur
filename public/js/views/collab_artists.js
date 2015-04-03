c493294d221ebe2755de06789a259690b27546f4
John Sanders <sandersjohnr@gmail.com>
2015-04-03 12:05:49 -0400

    main artist no longer shows up in collab view

--- public/js/views/collab_artists.js
+++ public/js/views/collab_artists.js
@@ -3,23 +3,28 @@ console.log('Loaded: views/collab_artists.js');
 App.Views.CollabArtists = Backbone.View.extend({
   initialize: function() {
     console.log('Created: CollabArtists Coll View');
-    this.listenTo(this.collection, 'reset', this.render);
+    // Render collection when main artist model is removed
+    this.listenTo(this.collection, 'remove', this.render);
   },
 
- 	el: '#collab-artist-container',
+  el: '#collab-artist-container',
 
   render: function() {
-  	this.collection.each(this.renderOne, this);
+    this.collection.each(this.renderOne, this);
   },
 
-  renderOne: function(collabArtistModel) {
-  	var collabArtist = new App.Views.CollabArtist({model: collabArtistModel});
-  	console.log(this);
-  	this.$el.append(collabArtist.$el);
+  renderOne: function(collabArtistModel) { 
+    var collabArtist = new App.Views.CollabArtist({ model: collabArtistModel });
+    console.log(this);
+    this.$el.append(collabArtist.$el);
   },
 
-  getCollabs: function(collabRoute) {
-  	this.collection.url = collabRoute;
-  	this.collection.fetch({reset: true});
+  getCollabs: function(collabRoute, mainArtistId) {
+    this.collection.url = collabRoute;
+    this.collection.fetch({ reset: true });
+    // Remove main artist model from collection
+    var mainArtistModel = this.collection.where({ artistId: mainArtistId });
+    this.collection.remove( mainArtistModel );
   }
+  
 });
