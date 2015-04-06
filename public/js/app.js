55bd3d2f8f3c20f4f97b6553a28d78ab43ccba1b
Emily Lordahl <contact@emilylordahl.com>
2015-04-06 00:45:45 -0400

    css stylization, some index.html and app.js changes

--- public/js/app.js
+++ public/js/app.js
@@ -15,3 +15,13 @@ $(function() {
   App.collabArtistsView = new App.Views.CollabArtists({collection: App.collabArtistsCollection});
   App.playlistView = new App.Views.Playlist({collection: App.playlistCollection});
 });
+
+var colors = ['slategray', 'darkslategray', 'dimgray', 'cornflowerblue', 'darkturquoise'];
+
+function switchColor () {
+  if (length >= colors.length) length = 0;
+  $('header').css('background-color', colors[length++]);
+  setTimeout(switchColor, 1200);
+};
+
+switchColor();
